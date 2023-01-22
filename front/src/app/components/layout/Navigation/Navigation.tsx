9f50e31254970337bcf0044c8bf6e285a5b6ceda
kyemets <yemetskonstantin@gmail.com>
2023-01-22 20:13:21 +0100

    added pages and styles

---
+++ front/src/app/components/layout/Navigation/Navigation.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+import styles from './Navigation.module.css';
+
+
+
+const Navigation: FC = () => {
+    return (
+        <div>Navigation</div>
+    )
+}
+
+export default Navigation;
---
+++ front/src/app/components/layout/Sidebar/Sidebar.tsx
@@ -0,0 +1,11 @@
+import styles from './Sidebar.module.css';
+
+
+
+const Sidebar = () => {
+    return (
+        <div>Sidebar</div>
+    )
+}
+
+export default Sidebar;
