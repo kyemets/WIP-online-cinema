4293cb59d0d3384ed48f8b84bb510e7f2484d166
kyemets <yemetskonstantin@gmail.com>
2023-02-08 12:56:37 +0100

    changed logo

--- front/src/app/components/layout/Navigation/Logo.tsx
+++ front/src/app/components/layout/Navigation/Logo.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { FC } from 'react';
-import logoImage from '../../../assets/images/logo3.svg';
+import logoImage from '../../../assets/images/logo.svg';
 
 const Logo: FC = () => {
     return (
